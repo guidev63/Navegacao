886d01031fbf0fad0dd83d7de3ecd269b42f83e5
Guilherme Augusto <guilhermedev23@gmail.com>
2026-01-05 22:55:15 -0300

    ok

---
+++ src/app/Home.tsx
@@ -0,0 +1,13 @@
+import { ButtonIcon } from "@/components/ButtonIcon";
+import { Header } from "@/components/Header";
+import { Title } from "@/components/Title";
+import { View } from "react-native";
+
+export function Home(){
+    return (<View style={{ flex:1, padding: 32 }}>
+     <Header>
+        <Title>Home</Title>
+        <ButtonIcon name="add-circle"></ButtonIcon>
+     </Header>
+    </View>)
+}
---
+++ src/components/ButtonIcon.tsx
@@ -0,0 +1,17 @@
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { MaterialIcons, Feather } from "@expo/vector-icons";
+
+
+
+type Props = TouchableOpacityProps & {
+    name: keyof typeof MaterialIcons.glyphMap
+}
+
+
+export function ButtonIcon({ name, ...rest }: Props) {
+    return (
+        <TouchableOpacity {...rest}>
+            <MaterialIcons name={name} size={24}  />
+        </TouchableOpacity>
+    )
+}
