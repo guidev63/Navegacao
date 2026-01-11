87eb478176b1924c0b5720246f28587d31166921
Guilherme Augusto <guilhermedev23@gmail.com>
2026-01-11 14:58:57 -0300

    ok

--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -1,20 +1,19 @@
 import { ButtonIcon } from "@/components/ButtonIcon";
 import { Header } from "@/components/Header";
 import { Title } from "@/components/Title";
-import {  StackRoutesProps } from "@/routes/StackRoutes";
+import { StackRoutesProps } from "@/routes/StackRoutes";
 import { View } from "react-native";
 
 
-export function Product({navigation,route}: StackRoutesProps<"product">) {
-    
+export function Product({ navigation, route }: StackRoutesProps<"product">) {
 
-    return (<View style={{ flex: 1, padding: 32,paddingTop: 54 }}
+
+    return (<View style={{ flex: 1, padding: 32, paddingTop: 54 }}
     >
         <Header>
             <ButtonIcon name="arrow-circle-left"
-             onPress={() => navigation.goBack()}></ButtonIcon>
+                onPress={() => navigation.goBack()}></ButtonIcon>
             <Title>Product {route.params?.id}</Title>
-            
         </Header>
     </View>
     )
--- src/routes/BottomRoutes.tsx
+++ src/routes/BottomRoutes.tsx
@@ -1,10 +1,10 @@
-import { createBottomTabNavigator,BottomTabScreenProps } from "@react-navigation/bottom-tabs";
+import { createBottomTabNavigator, BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { Home } from "@/app/Home";
 import { Product } from "@/app/Product";
-
+import { MaterialIcons } from "@expo/vector-icons";
 export type BottomRoutesList = {
     home: undefined;
-    product: undefined | { id:string}
+    product: undefined | { id: string }
 }
 
 export type BottomRoutesProps<T extends keyof BottomRoutesList> = BottomTabScreenProps<BottomRoutesList, T>;
@@ -17,8 +17,18 @@ export function BottomRoutes() {
             screenOptions={{ headerShown: false }}
         >
             <Tab.Screen name="home" component={Home}
+                options={{
+                    tabBarLabel: "Início",
+                    tabBarIcon: () => <MaterialIcons name="home" size={24} />,
+                }}
+
             />
             <Tab.Screen name="product" component={Product}
+                options={{
+                    tabBarLabel: "Produto",
+                    tabBarIcon: () => <MaterialIcons name="add-circle" size={24} />,
+
+                }}
             />
         </Tab.Navigator>
     );
